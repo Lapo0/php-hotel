3d5ba5e328c65d750aafcb8ae2bbcaf1222434be
Lapo0 <lapo.gini.01@gmail.com>
2023-03-20 15:13:38 +0100

    START

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php 
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    var_dump($_GET)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOTEL</title>
+</head>
+<body>
+
+    <h1>
+        HOTEL
+    </h1>
+
+    <ul>
+        <?php 
+            foreach ($hotels as $hotel) {
+        ?>
+
+        <li>
+            <?php 
+                echo $hotel
+            ?>
+        </li>
+        <?php 
+            }
+        ?>
+    </ul>
+    
+</body>
+</html>
